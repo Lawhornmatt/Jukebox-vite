a15ee1cda170b70592cd16422ef3a1c95a1bf11f
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-11-29 17:03:22 -0600

    added react styles to outside div

--- client/src/elements/VideoContainer.jsx
+++ client/src/elements/VideoContainer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const VideoContainer = ({ videos = [] }) => {
     return (
-        <div className="grid-container">
+        <div style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
             <Grid templateColumns='repeat(5, 1fr)' gap={6} m="4px" p="4px" w="50vw" overflowX="auto" overflowY="hidden" whiteSpace="nowrap">
                 {videos && videos.map((video) => {
                     <GridItem w='200px' h='100px' bg='blue.500'> {/* bg is for visual purposes; remove when thumbnail is available */}
