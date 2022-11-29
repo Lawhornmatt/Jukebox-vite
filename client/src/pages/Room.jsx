b0d1a0f8b7bdaf90cc41fbc1c6d148562239b61d
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-11-29 17:43:39 -0600

    added video list element to room element

--- client/src/pages/Room.jsx
+++ client/src/pages/Room.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import VideoList from '../elements/VideoList';
 
 const Room = () => {
-    ReactDOM.render(
-        <React.StrictMode>
-            <h1>Welcome ot the room page</h1>
-        </React.StrictMode>,
-        document.getElementById('root')
+    return (
+        <>
+            <h1>Welcome to the room page!</h1>
+            <VideoList />
+        </>
     )
 };
 
