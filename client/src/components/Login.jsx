9db19d9f78f7be4fd18ba8bb5de840ee4a48cf44
Thomas Le <tle1321@gmail.com>
2022-11-30 20:21:46 -0600

    woops

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -5,21 +5,19 @@ import { LOGIN_USER } from '../utils/mutations';
 
 import Auth from '../utils/auth';
 
-/*
 import {
   Flex,
   Box,
-  FormControl,
+
   FormLabel,
   Input,
   Checkbox,
   Stack,
-  Link,
   Button,
   Heading,
   useColorModeValue,
 } from '@chakra-ui/react';
-*/
+
 
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
@@ -55,12 +53,24 @@ const Login = (props) => {
       password: '',
     });
   };
-
   return (
-    <main className="flex-row justify-center mb-4">
-      <div className="col-12 col-lg-10">
-        <div className="card">
-          <h4 className="card-header bg-dark text-light p-2">Login</h4>
+    <Flex
+    minH={'100vh'}
+    align={'center'}
+    justify={'center'}
+    bg={useColorModeValue('gray.50', 'gray.800')}>
+
+      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+
+        <Stack align={'center'}>
+          <Heading fontSize={'4xl'}>Sign in to your account</Heading>
+        </Stack>
+
+          <Box
+            rounded={'lg'}
+            bg={useColorModeValue('white', 'gray.700')}
+            boxShadow={'lg'}
+            p={8}>
           <div className="card-body">
             {data ? (
               <p>
@@ -68,97 +78,47 @@ const Login = (props) => {
                 <Link to="/">back to the homepage.</Link>
               </p>
             ) : (
-              <form onSubmit={handleFormSubmit}>
-                <input
-                  className="form-input"
-                  placeholder="Your email"
+                <form spacing={4} onSubmit={handleFormSubmit}>
+
+                  <FormLabel>Email address</FormLabel>
+                  <Input 
                   name="email"
                   type="email"
                   value={formState.email}
                   onChange={handleChange}
                 />
-                <input
-                  className="form-input"
-                  placeholder="******"
+
+                  <FormLabel>Password</FormLabel>
+                  <Input 
                   name="password"
                   type="password"
                   value={formState.password}
                   onChange={handleChange}
                 />
-                <button
-                  className="btn btn-block btn-primary"
-                  style={{ cursor: 'pointer' }}
-                  type="submit"
-                >
-                  Submit
-                </button>
-              </form>
-            )}
 
-            {error && (
-              <div className="my-3 p-3 bg-danger text-white">
-                {error.message}
-              </div>
-            )}
-          </div>
-        </div>
-      </div>
-    </main>
-  );
-};
-
-export default Login;
-
-// Previous well stylized Login UI
-// We should use this but we'll need above's values and logic worked in
-// Note, uses a Link imported that conflicts with react-router-dom
-/* 
-export default function Login() {
-  return (
-    <Flex
-      minH={'100vh'}
-      align={'center'}
-      justify={'center'}
-      bg={useColorModeValue('gray.50', 'gray.800')}>
-      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-        <Stack align={'center'}>
-          <Heading fontSize={'4xl'}>Sign in to your account</Heading>
-        </Stack>
-        <Box
-          rounded={'lg'}
-          bg={useColorModeValue('white', 'gray.700')}
-          boxShadow={'lg'}
-          p={8}>
-          <Stack spacing={4}>
-            <FormControl id="email">
-              <FormLabel>Email address</FormLabel>
-              <Input type="email" />
-            </FormControl>
-            <FormControl id="password">
-              <FormLabel>Password</FormLabel>
-              <Input type="password" />
-            </FormControl>
             <Stack spacing={10}>
-              <Stack
-                direction={{ base: 'column', sm: 'row' }}
-                align={'start'}
-                justify={'space-between'}>
-                <Checkbox>Remember me</Checkbox>
-                <Link color={'blue.400'}>Forgot password?</Link>
-              </Stack>
               <Button
                 bg={'blue.400'}
                 color={'white'}
                 _hover={{
                   bg: 'blue.500',
-                }}>
+                      }}
+                      type="submit">
                 Sign in
               </Button>
             </Stack>
-          </Stack>
+                </form>
+              )}
+              {error && (
+                  <div className="my-3 p-3 bg-danger text-white">
+                    {error.message}
+                  </div>
+              )}
+            </div>
         </Box>
       </Stack>
     </Flex>
   );
 }
-*/
+
+export default Login;
