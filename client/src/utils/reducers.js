e6976079c301d86f8a445f796871bd54dc9c92f7
Lawhornmatt <66797498+Lawhornmatt@users.noreply.github.com>
2022-12-05 15:47:14 -0600

    Credit in Comments
    Added comments in the reducers.js file giving thanks to the tutorial and stackoverflow answer that got me here. Thank you WDS on YouTube and Nik from StackOverflow

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,4 +1,3 @@
-// import createId from './createId';
 import { LOGIN_USER, LOGOUT_USER } from './actions';
 
 export const initialState = { user: 'No User Logged In' };
@@ -20,3 +19,9 @@ export const reducer = (state, action) => {
     }
   }
 };
+
+// Credit:
+// A lot of ideas and help for implementing useContext came from here:
+// https://youtu.be/5LrDIWkK_Bc
+// And a lot of code structure and help for reducers/store came from this stackover answer from Nik:
+// https://stackoverflow.com/questions/57888975/how-to-update-react-context-provider-state-on-button-click
