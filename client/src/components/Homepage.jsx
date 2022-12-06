429d2d98cd8c868dc9905d0e4af14910520b3fa3
Lawhornmatt <66797498+Lawhornmatt@users.noreply.github.com>
2022-12-05 19:18:34 -0600

    LogIn Responsive Homepage

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -1,14 +1,22 @@
-import React, {useState, useEffect, useRef} from 'react'
-import {Image, Box, Text, Flex, useColorModeValue,Button} from '@chakra-ui/react'
-import NET from 'vanta/dist/vanta.net.min'
+import React, { useState, useEffect, useRef, useContext } from 'react';
+import {
+    Image, 
+    Box, 
+    Text, 
+    Flex, 
+    useColorModeValue,
+    Button } from '@chakra-ui/react';
+import NET from 'vanta/dist/vanta.net.min';
 import InitialFocus from './Modal';
 
 // User Context
-//import { accessUser } from '../utils/UserContext';
+import { UserContext } from '../utils/UserContext';
 
 
 export default function Homepage (){
-    //const { users, loginUser, logoutUser } = accessUser();  // Accesses the UserContext
+
+    const [ { user }, dispatch ] = useContext(UserContext);
+
     const [vantaEffect, setVantaEffect] = useState(0)
     const myRef = useRef(null)
     const color = useColorModeValue('blue', 'black')
--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {
     Modal,
     ModalOverlay,
@@ -9,62 +9,85 @@ import {
     ModalCloseButton,
     Button,
     useDisclosure,
-        FormControl,
-        FormLabel,
-        FormErrorMessage,
-        FormHelperText,
-        Input,
-        
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+    Input } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
-  } from '@chakra-ui/react'
-  import { Link } from 'react-router-dom';
+// User Context
+import { UserContext } from '../utils/UserContext';
 
 
 export default function InitialFocus() {
+
+    const [ { user }, dispatch ] = useContext(UserContext);
+
     const { isOpen, onOpen, onClose } = useDisclosure()
   
     const initialRef = React.useRef(null)
     const finalRef = React.useRef(null)
   
     return (
+    <>
+      {user ? (
+
       <>
-        <Button onClick={onOpen} colorScheme='yellow'>Click Here to Create a Room</Button>
-        <Link to='/signup'>
-        <Button ml={4} ref={finalRef}>
-          Create an Account
-        </Button>
-        </Link>
-  
-        <Modal
-          initialFocusRef={initialRef}
-          finalFocusRef={finalRef}
-          isOpen={isOpen}
-          onClose={onClose}
-        >
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>Create your account</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody pb={6}>
-              <FormControl>
-                <FormLabel>First name</FormLabel>
-                <Input ref={initialRef} placeholder='First name' />
-              </FormControl>
-  
-              <FormControl mt={4}>
-                <FormLabel>Last name</FormLabel>
-                <Input placeholder='Last name' />
-              </FormControl>
-            </ModalBody>
-  
-            <ModalFooter>
-              <Button colorScheme='blue' mr={3}>
-                Save
-              </Button>
-              <Button onClick={onClose}>Cancel</Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
+        {user.hosted_room ? (
+          <Link to='/room'>
+            <Button ml={4} ref={finalRef}>
+              Checkout Your Hosted Room
+            </Button>
+          </Link>
+        ) : (
+          <>
+            <Button onClick={onOpen} colorScheme='yellow'>Click Here to Create a Room</Button>
+
+
+            <Modal
+                initialFocusRef={initialRef}
+                finalFocusRef={finalRef}
+                isOpen={isOpen}
+                onClose={onClose}>
+
+                <ModalOverlay />
+                <ModalContent>
+                  <ModalHeader>Create your account</ModalHeader>
+                  <ModalCloseButton />
+                  <ModalBody pb={6}>
+                    <FormControl>
+                      <FormLabel>First name</FormLabel>
+                      <Input ref={initialRef} placeholder='First name' />
+                    </FormControl>
+
+                    <FormControl mt={4}>
+                      <FormLabel>Last name</FormLabel>
+                      <Input placeholder='Last name' />
+                    </FormControl>
+                  </ModalBody>
+
+                  <ModalFooter>
+                    <Button colorScheme='blue' mr={3}>
+                      Save
+                    </Button>
+                    <Button onClick={onClose}>Cancel</Button>
+                  </ModalFooter>
+                </ModalContent>
+            </Modal>
+          </>
+        )}
       </>
+
+      ) : (
+      <>
+        <Link to='/signup'>
+          <Button ml={4} ref={finalRef}>
+            Create an Account
+          </Button>
+        </Link>
+      </>
+      )}
+    </>
     )
   }
