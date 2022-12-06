ff19e4d944518d0bcc8425f977c6abe391bf89f0
Lawhornmatt <66797498+Lawhornmatt@users.noreply.github.com>
2022-12-06 16:33:36 -0600

    Undo Experiment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,28 +22,28 @@ import UserProvider from './utils/UserContext';
 
 // const port = Number.parseInt(process.env.PORT) || 3001;
 
-// let httpLink = createHttpLink({
-//   uri: 'http://localhost:3001/graphql'
-// });
+let httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql'
+});
 
-// if (process.env.NODE_ENV == 'production') {
-//   httpLink = createHttpLink({ uri: 'https://jukebox-project3.herokuapp.com/' });
-// };
+if (process.env.NODE_ENV == 'production') {
+  httpLink = createHttpLink({ uri: 'https://jukebox-project3.herokuapp.com/' });
+};
 
-// const authLink = setContext((_, { headers }) => {
-//   // get the authentication token from local storage if it exists
-//   const token = localStorage.getItem('id_token');
-//   // return the headers to the context so httpLink can read them
-//   return {
-//     headers: {
-//       ...headers,
-//       authorization: token ? `Bearer ${token}` : '',
-//     },
-//   };
-// });
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: 'https://jukebox-project3.herokuapp.com/',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
